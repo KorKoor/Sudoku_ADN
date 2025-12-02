b20c76e2a5948a77fe8730c5efca62bba3d57b38
Carlos <carlosghta9999@gmail.com>
2025-12-01 20:13:16 -0600

    Versión mejorada del algoritmo genético para Sudoku 9x9 con resumen optimizado

--- Sudoku_ADN.cpp
+++ Sudoku_ADN.cpp
@@ -3,174 +3,242 @@
 #include <ctime>
 #include <cstdlib>
 #include <algorithm>
+#include <numeric>
+#include <set>
+#include <locale.h>
 
 using namespace std;
 
-// Definimos el tama帽o del Sudoku (9x9)
 const int SIZE = 9;
+const int BLOCK_SIZE = 3;
+const int POPULATION_SIZE = 5000;
+const int MAX_GENERATIONS = 50000;
+const double MUTATION_RATE = 0.05;
 
-// Estructura para representar una soluci贸n de Sudoku como un "ADN"
+vector< vector<int> > initial_puzzle_ref;
+
 struct SudokuSolution {
-    vector<vector<int>> grid;
+    vector< vector<int> > grid;
     int fitness;
 
-    // Constructor para inicializar una soluci贸n aleatoria
     SudokuSolution() {
-        grid.resize(SIZE, vector<int>(SIZE, 0));
+        grid = initial_puzzle_ref;
+
         for (int i = 0; i < SIZE; i++) {
+            set<int> fixed_numbers;
+            vector<int> empty_cells_j;
+
             for (int j = 0; j < SIZE; j++) {
-                grid[i][j] = rand() % SIZE + 1;  // N煤meros aleatorios entre 1 y 9
+                if (grid[i][j] != 0) {
+                    fixed_numbers.insert(grid[i][j]);
+                } else {
+                    empty_cells_j.push_back(j);
+                }
+            }
+
+            vector<int> missing_numbers;
+            for (int k = 1; k <= SIZE; k++) {
+                if (fixed_numbers.find(k) == fixed_numbers.end()) {
+                    missing_numbers.push_back(k);
+                }
+            }
+
+            random_shuffle(missing_numbers.begin(), missing_numbers.end());
+            for (size_t k = 0; k < empty_cells_j.size(); k++) {
+                grid[i][empty_cells_j[k]] = missing_numbers[k];
             }
         }
         fitness = calculateFitness();
     }
 
-    // M茅todo para calcular la "aptitud" de la soluci贸n
-    int calculateFitness() {
-        int score = 0;
-        
-        // Verificar filas
-        for (int i = 0; i < SIZE; i++) {
-            vector<bool> seen(SIZE + 1, false);
-            for (int j = 0; j < SIZE; j++) {
-                if (seen[grid[i][j]]) {
-                    score--;
-                }
-                seen[grid[i][j]] = true;
-            }
-        }
+    // Evaluaci髇 de aptitud 
+    int calculateFitness() const {
+        int conflicts = 0;
 
-        // Verificar columnas
+        // Columnas
         for (int j = 0; j < SIZE; j++) {
-            vector<bool> seen(SIZE + 1, false);
+            set<int> seen;
             for (int i = 0; i < SIZE; i++) {
-                if (seen[grid[i][j]]) {
-                    score--;
+                if (seen.count(grid[i][j])) {
+                    conflicts++;
+                } else {
+                    seen.insert(grid[i][j]);
                 }
-                seen[grid[i][j]] = true;
             }
         }
 
-        // Verificar subcuadr铆culas de 3x3
-        for (int r = 0; r < SIZE; r += 3) {
-            for (int c = 0; c < SIZE; c += 3) {
-                vector<bool> seen(SIZE + 1, false);
-                for (int i = r; i < r + 3; i++) {
-                    for (int j = c; j < c + 3; j++) {
-                        if (seen[grid[i][j]]) {
-                            score--;
+        // Bloques 3x3
+        for (int r = 0; r < SIZE; r += BLOCK_SIZE) {
+            for (int c = 0; c < SIZE; c += BLOCK_SIZE) {
+                set<int> seen;
+                for (int i = r; i < r + BLOCK_SIZE; i++) {
+                    for (int j = c; j < c + BLOCK_SIZE; j++) {
+                        if (seen.count(grid[i][j])) {
+                            conflicts++;
+                        } else {
+                            seen.insert(grid[i][j]);
                         }
-                        seen[grid[i][j]] = true;
                     }
                 }
             }
         }
-
-        return score;
+        return conflicts;
     }
 
-    // M茅todo para imprimir el Sudoku
-    void printGrid() {
+    // Imprimir nuestro Sudoku
+    void printGrid() const {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                cout << grid[i][j] << " ";
+                cout << grid[i][j]
+                     << ((j + 1) % BLOCK_SIZE == 0 && j != SIZE - 1 ? " | " : " ");
             }
             cout << endl;
-        }
-    }
-
-    // M茅todo para mutar la soluci贸n
-    void mutate() {
-        int row = rand() % SIZE;
-        int col = rand() % SIZE;
-        grid[row][col] = rand() % SIZE + 1;
-        fitness = calculateFitness();
-    }
-
-    // M茅todo para cruzar dos soluciones
-    SudokuSolution crossover(const SudokuSolution& other) {
-        SudokuSolution child;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                // Cruzamos las soluciones mezclando filas de ambos padres
-                if (rand() % 2 == 0) {
-                    child.grid[i][j] = grid[i][j];
-                } else {
-                    child.grid[i][j] = other.grid[i][j];
-                }
+            if ((i + 1) % BLOCK_SIZE == 0 && i != SIZE - 1) {
+                cout << "---------------------" << endl;
             }
         }
-        child.fitness = child.calculateFitness();
-        return child;
     }
 };
 
-// Funci贸n para generar una poblaci贸n inicial
-vector<SudokuSolution> generatePopulation(int populationSize) {
-    vector<SudokuSolution> population;
-    for (int i = 0; i < populationSize; i++) {
-        population.push_back(SudokuSolution());
+// Cruce
+SudokuSolution crossover(const SudokuSolution& parent1, const SudokuSolution& parent2) {
+    SudokuSolution child = parent1;
+    int crossover_point = rand() % SIZE;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (i > crossover_point) {
+            child.grid[i] = parent2.grid[i];
+        }
     }
-    return population;
+    child.fitness = child.calculateFitness();
+    return child;
 }
 
-// Funci贸n para seleccionar los dos mejores individuos
-pair<SudokuSolution, SudokuSolution> selectParents(const vector<SudokuSolution>& population) {
-    SudokuSolution parent1 = population[0];
-    SudokuSolution parent2 = population[1];
+// Mutaci髇 
+void mutate(SudokuSolution& individual) {
+    if ((double)rand() / RAND_MAX > MUTATION_RATE) {
+        return;
+    }
 
-    for (const auto& individual : population) {
-        if (individual.fitness > parent1.fitness) {
-            parent1 = individual;
+    int row = rand() % SIZE;
+    vector<int> mutable_cols;
+    for (int j = 0; j < SIZE; j++) {
+        if (initial_puzzle_ref[row][j] == 0) {
+            mutable_cols.push_back(j);
         }
     }
 
-    for (const auto& individual : population) {
-        if (individual.fitness > parent2.fitness && individual.fitness != parent1.fitness) {
-            parent2 = individual;
+    if (mutable_cols.size() >= 2) {
+        int idx1 = rand() % mutable_cols.size();
+        int idx2 = rand() % mutable_cols.size();
+        while (idx1 == idx2) { idx2 = rand() % mutable_cols.size(); }
+
+        swap(individual.grid[row][mutable_cols[idx1]], individual.grid[row][mutable_cols[idx2]]);
+        individual.fitness = individual.calculateFitness();
+    }
+}
+
+// Selecci髇 por torneo 
+SudokuSolution tournamentSelection(const vector<SudokuSolution>& population, int tournamentSize = 5) {
+    SudokuSolution best = population[rand() % POPULATION_SIZE];
+    for (int i = 1; i < tournamentSize; i++) {
+        const SudokuSolution& current = population[rand() % POPULATION_SIZE];
+        if (current.fitness < best.fitness) {
+            best = current;
         }
     }
-
-    return {parent1, parent2};
+    return best;
 }
 
 int main() {
+    setlocale(LC_ALL, ""); // Para mostrar acentos correctamente
     srand(time(0));
+    cout << "--- Algoritmo Gen閠ico para Sudoku ---\n" << endl;
 
-    const int populationSize = 100;
-    const int generations = 1000;
+    // Sudoku inicial
+    int puzzle[9][9] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
 
-    // Poblaci贸n inicial
-    vector<SudokuSolution> population = generatePopulation(populationSize);
-
-    // Evoluci贸n
-    for (int generation = 0; generation < generations; generation++) {
-        // Selecci贸n de padres
-        auto parents = selectParents(population);
-        SudokuSolution parent1 = parents.first;
-        SudokuSolution parent2 = parents.second;
-
-        // Cruce y mutaci贸n
-        SudokuSolution child = parent1.crossover(parent2);
-        child.mutate();
-
-        // Reemplazo de un individuo con peor fitness
-        auto worstIt = min_element(population.begin(), population.end(), [](const SudokuSolution& a, const SudokuSolution& b) {
-            return a.fitness < b.fitness;
-        });
-        *worstIt = child;
-
-        // Imprimir soluci贸n si hemos encontrado una soluci贸n perfecta
-        if (child.fitness == SIZE * SIZE) {
-            cout << "隆Soluci贸n encontrada en la generaci贸n " << generation + 1 << "!" << endl;
-            child.printGrid();
-            break;
+    for (int i = 0; i < SIZE; i++) {
+        vector<int> row;
+        for (int j = 0; j < SIZE; j++) {
+            row.push_back(puzzle[i][j]);
         }
+        initial_puzzle_ref.push_back(row);
+    }
 
-        if (generation % 100 == 0) {
-            cout << "Generaci贸n: " << generation + 1 << " | Mejor aptitud: " << parent1.fitness << endl;
+    cout << "Puzzle Inicial:" << endl;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << initial_puzzle_ref[i][j]
+                 << ((j + 1) % BLOCK_SIZE == 0 && j != SIZE - 1 ? " | " : " ");
+        }
+        cout << endl;
+        if ((i + 1) % BLOCK_SIZE == 0 && i != SIZE - 1) {
+            cout << "---------------------" << endl;
+        }
+    }
+    cout << endl;
+
+    // Generar poblaci髇 inicial
+    vector<SudokuSolution> population;
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        population.push_back(SudokuSolution());
+    }
+
+    SudokuSolution bestSolution = population[0];
+    int totalMutations = 0;
+    int generationsUsed = 0;
+
+    for (int generation = 0; generation < MAX_GENERATIONS; generation++) {
+    generationsUsed = generation;
+
+    for (size_t i = 0; i < population.size(); i++) {
+        if (population[i].fitness < bestSolution.fitness) {
+            bestSolution = population[i];
         }
     }
 
-    return 0;
+    if (bestSolution.fitness == 0) {
+        cout << "OLUCI覰 ENCONTRADA en la Generaci髇: " << generation + 1 << "!" << endl;
+        break;
+    }
+
+    vector<SudokuSolution> newPopulation;
+    newPopulation.push_back(bestSolution);
+
+    while (newPopulation.size() < POPULATION_SIZE) {
+        SudokuSolution parent1 = tournamentSelection(population);
+        SudokuSolution parent2 = tournamentSelection(population);
+
+        SudokuSolution child = crossover(parent1, parent2);
+
+        int beforeFitness = child.fitness;
+        mutate(child);
+        if (child.fitness != beforeFitness) {
+            totalMutations++;
+        }
+
+        newPopulation.push_back(child);
+    }
+
+    population = newPopulation;
+	}
+
+	// Resumen final 
+	cout << "\n Resumen Final" << endl;
+	cout << "Generaciones ejecutadas: " << generationsUsed + 1 << endl;
+	cout << "Mutaciones totales: " << totalMutations << endl;
+
+	cout << "\nSudoku resultante:" << endl;
+	bestSolution.printGrid();
 }
